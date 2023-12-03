7cd86aaf84b5e5f70bb584622cd3950e2b58077a
josevanderley <89553942+josevanderley@users.noreply.github.com>
2023-12-03 15:39:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_03_While_Fatoral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro não negativo: ");
+            int numero;
+
+            // Certifique-se de que o usuário insira um número inteiro não negativo
+            while (!int.TryParse(Console.ReadLine(), out numero) || numero < 0)
+            {
+                Console.Write("Por favor, insira um número inteiro não negativo válido: ");
+            }
+
+            int fatorial = 1;
+            int i = 1;
+
+            // Calcula o fatorial usando uma estrutura de repetição do-while
+            do
+            {
+                fatorial *= i;
+                i++;
+            } while (i <= numero);
+
+            Console.WriteLine($"O fatorial de {numero} é {fatorial}");
+
+            Console.WriteLine("Digite 1 para sair");
+
+            Console.ReadKey();
+
+        }
+    }
+}
+   
